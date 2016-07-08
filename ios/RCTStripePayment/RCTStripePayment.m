f26dd762719d4dd9dc433858c7f5d1257320bf13
Wei Guangcheng <twksos@qq.com>
2016-07-08 12:10:38 -0400

    let react native do the payment

--- ios/RCTStripePayment/RCTStripePayment.m
+++ ios/RCTStripePayment/RCTStripePayment.m
@@ -19,23 +19,20 @@ RCT_EXPORT_METHOD(selectPayment:(NSDictionary *)options) {
     [self.paymentContext presentPaymentMethodsViewController];
 }
 
-RCT_EXPORT_METHOD(requestPayment:(NSDictionary *)options resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+RCT_EXPORT_METHOD(resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
     self.resolve = resolve;
     self.reject = reject;
     [self.paymentContext requestPayment];
 }
 
-- (void)paymentContextDidChange:(STPPaymentContext *)paymentContext {
-    NSLog(@"paymentContext %@", [paymentContext selectedPaymentMethod]);
-}
+- (void)paymentContextDidChange:(STPPaymentContext *)paymentContext {}
 
 - (void)paymentContext:(STPPaymentContext *)paymentContext didCreatePaymentResult:(STPPaymentResult *)paymentResult completion:(STPErrorBlock)completion {
-    NSLog(@"paymentResult %@");
-    self.resolve([[paymentResult source] stripeID]);
+    self.resolve(@{@"source" : [[paymentResult source] stripeID]});
 }
 
 - (void)paymentContext:(STPPaymentContext *)paymentContext didFinishWithStatus:(STPPaymentStatus)status error:(NSError *)error {
-    NSLog(@"didFinishWithStatus %@");
+    @throw [[NSError alloc] initWithDomain:@"DID_FINISH_WITH_STATUS_CALLED" code:@"100" userInfo:nil];
 }
 
 - (void)paymentContext:(STPPaymentContext *)paymentContext didFailToLoadWithError:(NSError *)error {

494f5966025c3e4e473515ef601f2979714cd4c8
Zhenjie Ruan <zhenjier@bu.edu>
2016-09-14 19:17:25 -0400

    Stripe Payment API android native module

---
+++ android/src/androidTest/java/io/liaoyuan/reactnative/stripepayment/ApplicationTest.java
@@ -0,0 +1,13 @@
+package io.liaoyuan.reactnative.stripepayment;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ android/src/main/java/io/liaoyuan/reactnative/stripepayment/StripePayment.java
@@ -0,0 +1,85 @@
+package io.liaoyuan.reactnative.stripepayment;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+
+import com.stripe.android.*;
+import com.stripe.android.model.Card;
+import com.stripe.android.model.Token;
+import com.stripe.exception.AuthenticationException;
+
+public class StripePayment extends ReactContextBaseJavaModule{
+
+    private static final String E_INVALID_PK = "E_INVALID_PK";
+    private static final String E_CARD_NUMBER_NOT_VALID = "E_CARD_NUMBER_NOT_VALID";
+    private static final String E_CARD_EXP_DATE_NOT_VALID = "E_CARD_EXP_DATE_NOT_VALID ";
+    private static final String E_CARD_CVC_NOT_VALID = "E_CARD_CVC_NOT_VALID";
+
+    public StripePayment(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "StripePayment";
+    }
+
+    @ReactMethod
+    public void getToken(ReadableMap cardInfo, final Promise promise) {
+        try{
+
+            String publicKey = cardInfo.getString("publicKey");
+
+            Stripe stripe = new Stripe(publicKey);
+
+            String number = cardInfo.getString("number");
+            int expMonth = cardInfo.getInt("expMonth");
+            int expYear = cardInfo.getInt("expDay");
+            String cvc = cardInfo.getString("cvc");
+
+            Card card = new Card.Builder(number, expYear, expMonth, cvc)
+                    .name(cardInfo.getString("name"))
+                    .addressLine1(cardInfo.getString("addressLine1"))
+                    .addressLine2(cardInfo.getString("addressLine2"))
+                    .addressCity(cardInfo.getString("city"))
+                    .addressState(cardInfo.getString("state"))
+                    .addressZip(cardInfo.getString("zip"))
+                    .addressCountry(cardInfo.getString("country"))
+                    .build();
+
+            if (!card.validateNumber()) {
+                promise.reject(E_CARD_NUMBER_NOT_VALID, "卡号有误");
+                return;
+            }
+
+            if (!card.validateExpiryDate()) {
+                promise.reject(E_CARD_EXP_DATE_NOT_VALID, "有效日期有误");
+                return;
+            }
+
+            if (!card.validateCVC()) {
+                promise.reject(E_CARD_CVC_NOT_VALID, "安全码有误");
+                return;
+            }
+
+            stripe.createToken(card,
+                    new TokenCallback() {
+                        @Override
+                        public void onError(Exception error) {
+                            promise.reject(error);
+                        }
+
+                        @Override
+                        public void onSuccess(Token token) {
+                            promise.resolve(token);
+                        }
+                    });
+
+        } catch(AuthenticationException e) {
+            promise.reject(E_INVALID_PK, "Invalid public key");
+        }
+    }
+}
